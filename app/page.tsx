ebdf9f86bf65a23f2c8c131d26bd4b2cb8cd0809
henrytnk <henrytnk@gmail.com>
2025-03-06 22:07:40 -0300

    Add Chibi Store Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { manrope } from "./_ui/fonts";
+import Image from "next/image";
 
 const HomePage = () => {
+
   return (
     <>
+      <Image 
+        src={"/chibi-store-logo.png"}
+        width={200}
+        height={200}
+        alt="Chibi Store Logo"
+      />
       <h1 className={`${manrope.className} text-2xl mb-2`}>Welcome to Chibi Store!</h1>
       <p className="mb-4">Authentic Japanese & Asian Goods, Shipped Worldwide.</p>
       <div className="flex gap-4">
