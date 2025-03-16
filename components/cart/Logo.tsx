eb4586ce684c3f8c9894af31fc4eb3439011c046
henrytnk <henrytnk@gmail.com>
2025-03-15 22:44:10 -0300

    Update Navbar Logo

--- components/cart/Logo.tsx
+++ components/cart/Logo.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
 const Logo = () => {
   return (
-    <Link href={"/"}>
-      <Image
-        src={"/chibi-store-logo.png"}
-        width={90}
-        height={90}
-        alt="Chibi Store Logo"
-      />
-    </Link>
+    <Button size='icon' asChild>
+      <Link href={"/"}>
+        <Image
+          src={"/chibi-store-logo.png"}
+          width={900}
+          height={900}
+          alt="Chibi Store Logo"
+        />
+      </Link>
+    </Button>
   );
 };
 
