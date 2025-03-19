0abbe6b30fbbd5bb977cc4449b4a883e37012168
henrytnk <henrytnk@gmail.com>
2025-03-19 15:23:59 -0300

    Add Cart Button

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,7 +1,25 @@
-const CartButton = () => {
-  return (
-    <div>CartButton</div>
-  )
-}
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { LuShoppingCart } from "react-icons/lu";
 
-export default CartButton
+const CartButton = () => {
+  const numItemsInCart = 10;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <LuShoppingCart />
+        <span className="flex items-center justify-center text-xs absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
+};
+
+export default CartButton;
